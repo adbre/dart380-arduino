08405778284606ec87a5132e5ace587a838cf5a1
Adam Brengesjö <adam.brengesjo@captario.com>
2016-10-20 14:57:07 +0200

    Remove console logging from index

--- index.js
+++ index.js
@@ -11,29 +11,3 @@ var dart380 = new Dart380({
         require('./lib/features/volume')
     ]
 });
-
-var eventBus = dart380.get('eventBus');
-
-eventBus.on('arduino.read', function (e) {
-    console.log('Read...: ', e.data);
-});
-
-eventBus.on('arduino.written', function (e) {
-    console.log('Written: ', e.data);
-});
-
-eventBus.on('arduino.ready', function (e) {
-    console.log('Ready');
-});
-
-eventBus.on('arduino.error', function (e) {
-    console.log('Error: ', e.error);
-});
-
-eventBus.on('arduino.disconnect', function (e) {
-    console.log('Disconnect: ', e.error);
-});
-
-eventBus.on('keyboard.keyPress', function (e) {
-    console.log('Keyboard event: ', e);
-});
